cbd0c87f9b71a8e2b016ea90cfd0eda0f0428432
Rick Kim <changjoon0604@gmail.com>
2018-11-07 13:58:30 +0800

    update and split into differet classes

---
+++ New_Dijkstra/src/Edge.java
@@ -0,0 +1,10 @@
+
+//define Edge
+class Edge{
+	public final Node target;
+	public final double weight;
+	public Edge(Node targetNode, double weightVal){
+		target = targetNode;
+		weight = weightVal;
+	}
+}
---
+++ New_Dijkstra/src/Node.java
@@ -0,0 +1,21 @@
+//define Node
+class Node implements Comparable<Node>{
+	
+	public final String value;
+	public Edge[] adjacencies;
+	public double shortestDistance = Double.POSITIVE_INFINITY;
+	public Node parent;
+
+	public Node(String val){
+		value = val;
+	}
+
+	public String toString(){
+			return value;
+	}
+
+	public int compareTo(Node other){
+		return Double.compare(shortestDistance, other.shortestDistance);
+	}
+
+}
